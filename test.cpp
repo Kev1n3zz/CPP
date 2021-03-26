3effe28d52e3dba8d396fb325378b9c93c36fc8d
HengjiuLiu <2070436220@szu.edu.cn>
2021-03-26 21:41:39 +0800

    C++demo

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// 这是一个调试测试文件，您可以直接运行检测配置是否生效
+// 若配置任然无法调试，请查阅README_inProject.md
+
+#include <stdio.h>
+#include <stdlib.h>
+//在使用printf语句时候，/n只能输入英文字母
+int main() {
+    printf("This is my first c plus program and in this program i'll test is a sign can show in the sentence like \n");
+    system("pause");
+    return 0;
+} 
