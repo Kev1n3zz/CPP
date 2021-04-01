f21a06f32c523979b5004ab512dc394f7893e20e
HengjiuLiu <2070436220@szu.edu.cn>
2021-04-01 21:15:50 +0800

    vector pointer

---
+++ pointerVector.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+//一个指向vector<int>的指针
+//用&获取地址
+int main(){
+    using namespace std;
+    
+    vector<int> *pv=0;//调用一个vector<int> 类型的指针
+    //pv= &vectorname; 可以如此调用一个vector的地址
+    system("pause");
+    return 0;
+}
